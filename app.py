173b122d7820c2168230a3b07c05749de9f3d51f
Meyer <ehlmeyer@gmail.com>
2019-08-13 16:21:15 -0500

    correlations between air force one and contact

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 from flask import Flask, jsonify, request
 import pandas as pd
 import numpy as mp
@@ -8,9 +10,65 @@ warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 print('test')
+# data frame read the csv, data is separated by commas
 df = pd.read_csv('./data/ratings.csv', sep=',')
-print(df.head())
+# shows first 5 rows 
+df.head()
+# read movies.csv
 movie_titles = pd.read_csv('./data/movies.csv', sep=',')
-print(movie_titles.head())
+movie_titles.head()
+# ratings.csv and movies.csv
 df = pd.merge(df, movie_titles, on='movieId')
-print(df.head())
+# print(df.head())
+# show stats from dataset
+# print(df.describe())
+# return DataFrame of average rating for each title
+ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
+# print(ratings.head())
+# create a column to display number of ratings
+ratings['number_of_ratings'] = df.groupby('title')['rating'].count()
+# print(ratings.head)
+
+# load histogram of ratings
+plt.hist(ratings['rating'], bins=50)
+# histo of number of ratings
+plt.hist(ratings['number_of_ratings'], bins=60)
+# use seabord to scatter plot number of ratings and rating
+sns.jointplot(x='rating', y='number_of_ratings', data=ratings)
+# plt.show()
+
+# create a matrix with movie titles as columns, userId as indexes, and ratings as values
+movie_matrix = df.pivot_table(index='userId', columns='title', values='rating')
+# print(movie_matrix.head())
+
+# show the ten movies with the most ratings
+# print(ratings.sort_values('number_of_ratings', ascending=False).head(10))
+
+# show all user ratings for Air Force One and Contact
+AFO_user_rating = movie_matrix['Air Force One (1997)']
+contact_user_rating = movie_matrix['Contact (1997)']
+# print(AFO_user_rating.head(10), contact_user_rating.head(10))
+
+# correlation to AFO
+similar_to_AFO = movie_matrix.corrwith(AFO_user_rating)
+# print(similar_to_AFO.head())
+
+# correlation to contact
+similar_to_contact = movie_matrix.corrwith(contact_user_rating)
+# print(similar_to_contact.head())
+
+# drop null values from matrix and transform correlation results into DataFrames
+corr_contact = pd.DataFrame(similar_to_contact, columns = ['Correlation'])
+corr_contact.dropna(inplace=True)
+# print(corr_contact.head())
+
+corr_AFO = pd.DataFrame(similar_to_AFO, columns = ['Correlation'])
+corr_AFO.dropna(inplace=True)
+# print(corr_AFO.sort_values('Correlation', ascending=False).head())
+
+# set a threshold for number of ratings
+
+# join correlations with number of ratings column in the ratings datafield
+corr_AFO = corr_AFO.join(ratings['number_of_ratings'])
+corr_contact = corr_contact.join(ratings['number_of_ratings'])
+print(corr_AFO.head(), corr_contact.head())
