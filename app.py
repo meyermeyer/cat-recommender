f58de8700a8915d06ce1e1991a64882d0e21e122
Meyer <ehlmeyer@gmail.com>
2019-08-15 16:05:47 -0500

    sending recommendations to client

--- app.py
+++ app.py
@@ -14,14 +14,54 @@ api = Api(app)
 
 print('test')
 
+# data frame read the csv, data is separated by commas
+df = pd.read_csv('./data/ratings.csv', sep=',')
+# shows first 5 rows
+df.head()
+
+# read movies.csv
+movie_titles = pd.read_csv('./data/movies.csv', sep=',')
+# print(movie_titles.head())
+# ratings.csv and movies.csv
+df = pd.merge(df, movie_titles, on='movieId')
+
+# show stats from dataset
+# print(df.describe())
+
+# return DataFrame of average rating for each title
+ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
+
+# create a column to display number of ratings
+ratings['number_of_ratings'] = df.groupby('title')['rating'].count()
+
+# create a matrix with movie titles as columns, userId as indexes, and ratings as values
+movie_matrix = df.pivot_table(index='userId', columns='title', values='rating')
+# print(movie_matrix)
 
 class Recommendations(Resource):
     def get(self, title):
-        print(title)
-        return {'hello': 'world'}
+        
+        # all ratings for selected title
+        title_user_rating = movie_matrix[title]
+        # print(title_user_rating)
+        # correlation to title
+        similar_to_title = movie_matrix.corrwith(title_user_rating)
+        print(similar_to_title)
+        # drop null values from matrix and transform correlation results into DataFrame
+        corr_title = pd.DataFrame(
+            data=similar_to_title, columns=['Correlation'])
+        corr_title.dropna(inplace=True)
+        # join correlations with number of ratings column in the ratings datafield
+        corr_title = corr_title.join(ratings['number_of_ratings'])
+        # set a threshold for number of ratings
+        top_ten = corr_title[corr_title['number_of_ratings']>100].sort_values(by='Correlation', ascending=False).head(10)
+        json_object = top_ten.to_dict('index')
+        print(json_object)
+        return {'recommendations': json_object}
+        # return {'hello': 'world'}
 
 
-api.add_resource(Recommendations, '/movies/<string:title>')
+api.add_resource(Recommendations, '/movies/recommendations/<string:title>')
 
 class Movies(Resource):
     def get(self):
@@ -30,40 +70,23 @@ class Movies(Resource):
         # print(movie_titles)
         return jsonify({'movie_titles': movie_titles})
         # return{'movie_titles': movie_titles}
-        # return {'hello': 'world'}
+        
 
 api.add_resource(Movies, '/movies')
 
-# data frame read the csv, data is separated by commas
-df = pd.read_csv('./data/ratings.csv', sep=',')
-# shows first 5 rows 
-df.head()
-# read movies.csv
-movie_titles = pd.read_csv('./data/movies.csv', sep=',')
-# print(movie_titles.head())
-# ratings.csv and movies.csv
-df = pd.merge(df, movie_titles, on='movieId')
-# print(df.head())
-# show stats from dataset
-# print(df.describe())
-# return DataFrame of average rating for each title
-ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
-# print(ratings.head())
-# create a column to display number of ratings
-ratings['number_of_ratings'] = df.groupby('title')['rating'].count()
-# print(ratings.head)
+
+
 
 # load histogram of ratings
-plt.hist(ratings['rating'], bins=50)
+# plt.hist(ratings['rating'], bins=50)
 # histo of number of ratings
-plt.hist(ratings['number_of_ratings'], bins=60)
+# plt.hist(ratings['number_of_ratings'], bins=60)
 # use seabord to scatter plot number of ratings and rating
-sns.jointplot(x='rating', y='number_of_ratings', data=ratings)
+# sns.jointplot(x='rating', y='number_of_ratings', data=ratings)
 # plt.show()
 
-# create a matrix with movie titles as columns, userId as indexes, and ratings as values
-movie_matrix = df.pivot_table(index='userId', columns='title', values='rating')
-# print(movie_matrix.head())
+
+
 
 # show the ten movies with the most ratings
 # print(ratings.sort_values('number_of_ratings', ascending=False).head(10))
