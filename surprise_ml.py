f5a2dae7a03bd96f28002dd28ef940c4afb0274c
Meyer <ehlmeyer@gmail.com>
2019-08-19 20:57:16 -0500

    finally got predictions in a dict

--- surprise_ml.py
+++ surprise_ml.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from surprise import Reader, Dataset
+from surprise.model_selection import train_test_split
 from surprise import SVD, evaluate
 from surprise import NMF
+from collections import defaultdict
 import pandas as pd
 
 
@@ -11,31 +13,52 @@ app = Flask(__name__)
 print('surprise test')
 
 # data frame read the csv, data is separated by commas
-ratings = pd.read_csv('./data/ratings.csv', sep=',')
-ratings.head()
+df = pd.read_csv('./data/ratings.csv', sep=',')
 
-ratings_dict = {
-    'itemID': list(ratings.movieId),
-    'userID': list(ratings.userId),
-    'rating': list(ratings.rating)
-}
+# read movies.csv
+movie_titles = pd.read_csv('./data/movies.csv', sep=',')
 
-df = pd.DataFrame(ratings_dict)
+# ratings.csv and movies.csv
+df = pd.merge(df, movie_titles, on='movieId')
 
-reader = Reader(rating_scale=(0.5,5.0))
+# return DataFrame of average rating for each title
+ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
 
-data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
+# create a column to display number of ratings
+ratings['number_of_ratings'] = df.groupby('title')['rating'].count()
 
-# split data into 5 folds
-data.split(n_folds=5)
+# create a matrix with movie titles as columns, userId as indexes, and ratings as values
+movie_matrix = df.pivot_table(index='userId', columns='title', values='rating')
+# movie_matrix = movie_matrix.fillna()
+print(movie_matrix.head())
 
-# SVD - Singular Value Decomposition
+
+
+
+data = Dataset.load_builtin('ml-100k')
+trainset = data.build_full_trainset()
 algo = SVD()
-evaluate(algo, data, measures=['RMSE'])
+algo.fit(trainset)
+
+testset = trainset.build_anti_testset()
+predictions = algo.test(testset)
+all_predictions = {}
+all_predictions['uid'] = {}
+all_predictions['uid']['iid'] = 'est'
+# each_rating = {'iid' : 'est'}
+# prediction_by_uid = defaultdict(list)
+# all_predictions = {'uid' : {'iid' : 'est'}}
+for uid, iid, true_r, est, _ in predictions:
+    all_predictions[uid] = {} 
+    all_predictions[uid][iid] = est
+    # all_predictions[uid] = 
+
+
+print(all_predictions)
 
 # NMF - non negative matrix factorization
 algo = NMF()
-evaluate(algo, data, measures=['RMSE'])
+# evaluate(algo, data, measures=['RMSE'])
 
 
 if __name__ == '__main__':
